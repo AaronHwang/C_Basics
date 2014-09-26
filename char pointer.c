5a658094ab40ef3411fc1e3e5cdd17ee08a17401
AaronHwang <aaronhwang@foxmail.com>
2014-09-26 22:26:19 +0800

    update

--- char pointer.c
+++ char pointer.c
@@ -19,5 +19,16 @@ int main(void)
 		printf("%s   ", ch + i);//注意是%s，不能是%c
 		printf("%s   \n", s + i);//注意是%s
 	}
+
+  	char *s2 = malloc((strlen(s)+1)*sizeof(char));//从堆heap分配内存并返回改内存地址
+  	// char *s2 ;s2 = malloc((strlen(s)+1)*sizeof(char));
+  	//同理，char *s2 = "Aaron";
+
+  	s2 = s;//复制s赋值给s2
+  	printf("%s\n", s2);
+
+  	s2 = "Aaron";//分配内存后可以赋值
+  	printf("\n%s\n", s2);
+
 	return 0;
 }
