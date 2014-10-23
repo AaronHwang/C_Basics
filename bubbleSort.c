2c80a0246cd11c49da389a64edde537509f5cd8f
AaronHwang <aaronhwang@foxmail.com>
2014-10-23 12:26:19 +0800

    增加判断排序是否提前完成

--- bubbleSort.c
+++ bubbleSort.c
@@ -5,14 +5,16 @@
 void bubble_sort(int array[], int length)
 {
 	int i = 0, j = 0, tmp = 0;
+	int flag = 1;//判断是否已经排序成功
 
 	if ( NULL == array || 0 == length)//这样写条件判断可以方式误赋值！
 	{
 		return;
 	}
 
-	for (int i = 0; i < length - 1; i++)
+	for (int i = 0; i < length - 1 && flag; i++)
 	{
+		flag = 0;
 		for (int j = 0; j < length - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,6 +22,7 @@ void bubble_sort(int array[], int length)
 				tmp = array[j];
 				array[j] = array[j+1];
 				array[j+1] = tmp;
+				flag = 1;
 			}
 		}
 
